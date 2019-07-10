300a0782f2d9bc73245d4edd10dc74eba736b61b
sprea22 <a.spreafico13@campus.unimib.it>
2019-07-10 11:57:36 -0700

    First commit: still have to implement the main, but all the single components have been implemented at least as V1

---
+++ main.py
@@ -0,0 +1,7 @@
+
+# STILL TO IMPLEMENT THE MAIN #
+
+# It has to:
+# 1) call the classifier and return the prediction
+# 2) call the text_extractor and return the title
+# 3) call the LSTM, giving in input just the classifier output (for the moment) well formatted.
